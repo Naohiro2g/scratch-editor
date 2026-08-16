13f8ec631171777a2d34a97f6d522bf28152e72e
Naohiro2g <587586+Naohiro2g@users.noreply.github.com>
2026-08-16 12:14:50 +0900

    fix(gui): tolerate missing MessageChannel

--- packages/scratch-gui/src/lib/mcremote-wirescope-source.js
+++ packages/scratch-gui/src/lib/mcremote-wirescope-source.js
@@ -330,7 +330,7 @@ const createWireScopeSource = function (environment) {
 
 const defaultEnvironment = () => ({
     window,
-    MessageChannel,
+    MessageChannel: window.MessageChannel,
     crypto,
     now: Date.now,
     setTimeout,
